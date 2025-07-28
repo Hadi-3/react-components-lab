d0b39940bded848303e7a2ac71a7161e616afad0
hadi osama <hadiosama622@gmail.com>
2025-07-28 13:08:13 +0300

    almos solved

---
+++ WeatherForecast.jsx
@@ -0,0 +1,13 @@
+const WeatherForecasts = (props) => {
+    const {day, img, imgAlt, conditions, time} = props;
+    return (
+ <div className="weather">
+  <h2>{day}</h2>
+  <img src={img} alt={imgAlt} />
+  <p><span>{conditions} </span>current weather conditions</p>
+  <p><span>{time} </span>time of day</p>
+</div>
+    )
+}
+
+export default WeatherForecasts;
